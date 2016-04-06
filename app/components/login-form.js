0fb006e905348ac86cb4c2bd103d26726db64fda
Kyle Deal <kdeal@kyledeal.com>
2016-04-06 12:25:42 -0400

    Made the login form work

---
+++ app/components/login-form.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+import {
+  validator, buildValidations
+}
+from 'ember-cp-validations';
+
+var Validations = buildValidations({
+    username: [
+        validator('presence', true),
+        validator('length', {
+            min: 6,
+            max: 30
+        }),
+    ],
+    password: validator('presence', true),
+});
+
+export default Ember.Component.extend(Validations, {
+    session: Ember.inject.service('session'),
+    username: '',
+    password: '',
+    error_msg: false,
+
+    actions: {
+        login() {
+            this.get('session').authenticate('authenticator:local', this.username, this.password).catch((reason) => {
+                this.set('error_msg', reason);
+                return true;
+            });
+        },
+    },
+});
