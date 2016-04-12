a59168c5b04b87e75b8052ac07c3d1b8f498d042
Kyle Deal <kdeal@kyledeal.com>
2016-04-12 00:48:43 -0400

    Added page plugins

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
   this.route('register');
   this.route('create-project', {path: '/create'});
   this.route('create-category', {path: '/:username/:project/category/create'});
+  this.route('create-page-plugin', {path: '/:username/:project/page_plugin/create'});
 });
 
 export default Router;
