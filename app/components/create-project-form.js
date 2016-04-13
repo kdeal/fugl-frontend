46a728ae4a124a3f83c79e4cf3523df2b1b9820c
Kyle Deal <kdeal@kyledeal.com>
2016-04-12 20:26:38 -0400

    Made basic form component to be used by form components

--- app/components/create-project-form.js
+++ app/components/create-project-form.js
@@ -1,11 +1,3 @@
-import Ember from 'ember';
+import BasicForm from 'fugl-frontend/components/basic-form';
 
-export default Ember.Component.extend({
-    actions: {
-        submit() {
-            this.get('model').save();
-            this.sendAction();
-            return true;
-        },
-    },
-});
+export default BasicForm.extend();
