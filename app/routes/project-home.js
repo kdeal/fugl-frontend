60c23d5add7f6dd1bae983a09c6722107fa2971c
Kyle Deal <kdeal@kyledeal.com>
2016-04-19 01:32:17 -0400

    Add project-plugins to project-home

--- app/routes/project-home.js
+++ app/routes/project-home.js
@@ -10,6 +10,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, ModelProjectLink, {
                 project: this.getProject(params.project, params.username),
                 categories: this.get_existing(params.project, params.username, 'category'),
                 page_plugins: this.get_existing(params.project, params.username, 'page_plugin'),
+                project_plugins: this.get_existing(params.project, params.username, 'project_plugin'),
                 pages: this.get_existing(params.project, params.username, 'page'),
                 posts: this.get_existing(params.project, params.username, 'post'),
             };
@@ -17,6 +18,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, ModelProjectLink, {
                 var model = results.project;
                 model.categories = results.categories;
                 model.page_plugins = results.page_plugins;
+                model.project_plugins = results.project_plugins;
                 model.pages = results.pages;
                 model.posts = results.posts;
                 run(null, resolve, model);
