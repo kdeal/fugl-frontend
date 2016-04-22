cb31cd2f2a7ee259c32b3fd798c3a9b12f27234a
Kyle Deal <kdeal@kyledeal.com>
2016-04-22 10:53:13 -0400

    Forgot to remove console log

--- app/components/basic-form.js
+++ app/components/basic-form.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
         submit() {
-            console.log(this.get('model.theme'));
             this.get('model').save().then(() => {
                 this.sendAction();
             });
