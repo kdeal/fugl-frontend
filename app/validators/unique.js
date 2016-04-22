5c94393ed2788f0c2a5ee7ef1c24d25d8953969f
Kyle Deal <kdeal@kyledeal.com>
2016-04-22 02:52:46 -0400

    Check that the value has changed from what it originally was

--- app/validators/unique.js
+++ app/validators/unique.js
@@ -7,11 +7,22 @@ export default BaseValidator.extend({
         if (!value) {
             return false;
         }
+        var dirty_attr = model.changedAttributes();
+        if (!dirty_attr[options['field']]) {
+            return true;
+        }
+        // [old_value, new_vale]
+        var dirty_elem = dirty_attr['field'];
+        // old value is the same as the current one
+        if (dirty_elem[0] === value) {
+            return true;
+        }
         var server_query = {};
         server_query[options['field']] = value;
         for (var key in options['filter']) {
                 server_query[key] = model.get(key);
         }
+
         var query = {};
         var plural_model = options['model'].pluralize();
         query[plural_model + '/available'] = server_query;
