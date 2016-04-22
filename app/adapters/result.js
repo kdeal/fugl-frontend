692843a87c0851a98f6eb6760eb19f1d2fc0885e
Kyle Deal <kdeal@kyledeal.com>
2016-04-22 00:03:36 -0400

    Remove duplicate function

--- app/adapters/result.js
+++ app/adapters/result.js
@@ -7,23 +7,4 @@ export default ApplicationAdapter.extend({
         url += key + "/";
         return url;
     },
-    sortQueryParams(obj) {
-        var keys = Object.keys(obj);
-        var len = keys.length;
-        if (len === 0) {
-            return obj;
-        } else if (len === 1 && obj[keys[0]].constructor === Object) {
-            obj = obj[keys[0]];
-            keys = Object.keys(obj);
-            len = keys.length;
-        }
-
-        var newQueryParams = {};
-        var sortedKeys = keys.sort();
-
-        for (var i = 0; i < len; i++) {
-            newQueryParams[sortedKeys[i]] = obj[sortedKeys[i]];
-        }
-        return newQueryParams;
-    },
 });
